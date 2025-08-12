bfae756b787a79b7093efd0e0d1c308912af50aa
Xojiakbar <xojiakbar0719@gmail.com>
2025-08-12 22:50:02 +0500

    small update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Exchange Rates Bot',
+  title: 'Exchange Rates Bot ',
   description: 'Get live exchange rates from multiple banks in Uzbekistan',
 };
 
